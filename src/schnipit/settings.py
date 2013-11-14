d05348dd9822659444d7862325b3964e1a4e3faf
James ‘Twey’ Kay <twey@twey.co.uk>
2013-11-14 02:17:01 +0000

    Add django_extensions where supported

--- src/schnipit/settings.py
+++ src/schnipit/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+import sys
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -37,6 +38,9 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 )
+
+# Werkzeug doesn't werk zeug good on Python 3.2.
+if DEBUG and sys.version_info > (3, 2): INSTALLED_APPS += ('django_extensions',)
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
