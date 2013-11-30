279032eab9583a5d2d6264da1536ffb76df49e53
James ‘Twey’ Kay <twey@twey.co.uk>
2013-11-30 09:06:59 +0000

    Switch back to static pipeline

--- src/shnipit/settings.py
+++ src/shnipit/settings.py
@@ -85,7 +85,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = "/static/"
-
-STATICFILES_STORAGE="pipeline.storage.PipelineCachedStorage"
-
-from .settings_pipeline import *
