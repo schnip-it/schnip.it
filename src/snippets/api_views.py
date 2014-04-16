c1e52da6ca6a204812a5d57a5be78c4159c975f7
James ‘Twey’ Kay <twey@twey.co.uk>
2014-04-16 04:17:45 +0100

    Add REST API

---
+++ src/snippets/api_views.py
@@ -0,0 +1,11 @@
+from .models import Snippet, Board
+from .serializers import SnippetSerializer
+from rest_framework import generics
+
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
---
+++ src/snippets/serializers.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django.forms import widgets
+from rest_framework import serializers
+from .models import Snippet
+
+    # creation_time = models.DateField(auto_now_add=True)
+    # title = models.CharField(max_length=255)
+    # description = models.TextField()
+    # owner = models.ForeignKey(User, related_name="snippets")
+    # board = models.ForeignKey(Board, related_name="snippets")
+    # language = models.CharField(max_length=255, choices=SNIPPET_LANGUAGES)
+    # code = models.TextField()
+    # tags = TaggableManager()
+    # ratings = GenericRelation(Rating)
+
+
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ["id", "title", "description", "owner", "board", "language", "code"]
